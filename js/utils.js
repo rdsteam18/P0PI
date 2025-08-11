0112d0b83587bf17fdd57c1a2c982eaf40cbb508
rdsteam18 <149048516+rdsteam18@users.noreply.github.com>
2025-08-11 21:13:29 +0530

    Delete js directory

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-// utils.js
-export function escapeHtml(str) {
-  if (!str) return '';
-  return String(str).replace(/[&<>"']/g, c => ( {
-    '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'
-  }[c]));
-}
